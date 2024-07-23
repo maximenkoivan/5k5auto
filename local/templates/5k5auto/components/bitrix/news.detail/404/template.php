4cb432d38c48fa36a05389f1f0842fd146e04014
maximenko <maximenkoivan83@gmail.com>
2024-07-23 17:02:50 +0300

    выносит в админку настройки страницы 404

--- local/templates/5k5auto/components/bitrix/news.detail/404/template.php
+++ local/templates/5k5auto/components/bitrix/news.detail/404/template.php
@@ -9,9 +9,9 @@ $this->addExternalCss(SITE_TEMPLATE_PATH . '/assets/css/404.css');
 <div class="absent">
     <div class="container">
         <div class="absent__content">
-            <h1 class="absent__title">404</h1>
-            <p class="absent__text">Страница не найдена</p>
+            <h1 class="absent__title"><?= $arResult['PROPERTIES']['TITLE_404']['~VALUE'] ?></h1>
+            <p class="absent__text"><?= $arResult['PROPERTIES']['DESC_404']['~VALUE'] ?></p>
         </div>
-        <a class="absent__btn" href="/" target="_blank" rel="noopener noreferrer">На главную</a>
+        <a class="absent__btn" href="/" target="_blank" rel="noopener noreferrer"><?= $arResult['PROPERTIES']['TEXT_LINK_404']['~VALUE'] ?></a>
     </div>
 </div>
