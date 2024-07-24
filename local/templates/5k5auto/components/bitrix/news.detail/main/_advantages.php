73db89e880b6e9c230169a1a0b0419c0236ef9e5
maximenko <maximenkoivan83@gmail.com>
2024-07-24 16:18:58 +0300

    добавляет блок "преимущества"

--- local/templates/5k5auto/components/bitrix/news.detail/main/_advantages.php
+++ local/templates/5k5auto/components/bitrix/news.detail/main/_advantages.php
@@ -4,60 +4,39 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
  * @var $arResult
  * @global CMain $APPLICATION
  */
+$advantages = \classes\Models\FiveKFiveAuto\Content\Advantages::getInstance()->getElements()->getResult();
 ?>
-<section class="advantages">
-    <div class="container">
-        <h2 class="advantages__title">Преимущества</h2>
-        <div class="advantages__list advantages-cards">
-            <div class="advantages-cards__item">
-                <div class="advantages-cards__img">
-                    <img width="126" height="126" src="images/advantages_1.svg" alt="Картинка">
-                </div>
-                <p class="advantages-cards__text">
-                    Оперативная помощь в подборе пленки для вашего авто
-                </p>
-            </div>
-            <div class="advantages-cards__item">
-                <div class="advantages-cards__img">
-                    <img width="90" height="90" src="images/advantages_2.svg" alt="Картинка">
-                </div>
-                <p class="advantages-cards__text">
-                    Одна из лучших<br> цен на рынке
-                </p>
-            </div>
-            <div class="advantages-cards__item">
-                <div class="advantages-cards__img">
-                    <img width="126" height="126" src="images/advantages_3.svg" alt="Картинка">
-                </div>
-                <p class="advantages-cards__text">
-                    Без затягивания<br> сроков
-                </p>
-            </div>
-            <div class="advantages-cards__item">
-                <div class="advantages-cards__img">
-                    <img width="90" height="90" src="/images/advantages_4.svg" alt="Картинка">
-                </div>
-                <p class="advantages-cards__text">
-                    Гарантия качества<br>
-                    и результата
-                </p>
-            </div>
-            <div class="advantages-cards__item">
-                <div class="advantages-cards__img">
-                    <img width="126" height="126" src="images/advantages_5.svg" alt="Картинка">
-                </div>
-                <p class="advantages-cards__text">
-                    Сертифицированный<br> материал
-                </p>
-            </div>
-            <div class="advantages-cards__item">
-                <div class="advantages-cards__img">
-                    <img width="90" height="90" src="images/advantages_6.svg" alt="Картинка">
-                </div>
-                <p class="advantages-cards__text">
-                    Индивидуальные<br> решения
-                </p>
+<?php if (!empty($advantages)): ?>
+    <section class="advantages">
+        <div class="container">
+            <h2 class="advantages__title"><?= $arResult['PROPERTIES']['TITLE_ADVANTAGES']['~VALUE'] ?></h2>
+            <div class="advantages__list advantages-cards">
+                <?php foreach ($advantages as $key => $advantage): ?>
+                    <?php if (empty($advantage['IMAGE']['VALUE']) || empty($advantage['DESCRIPTION']['~VALUE'])) continue ?>
+                    <?php if ($key % 2 === 0): ?>
+                        <div class="advantages-cards__item">
+                            <div class="advantages-cards__img">
+                                <img width="126" height="126" src="<?= CFile::GetPath($advantage['IMAGE']['VALUE']) ?>"
+                                     alt="<?= $advantage['DESCRIPTION']['~VALUE'] ?>">
+                            </div>
+                            <p class="advantages-cards__text">
+                                <?= $advantage['DESCRIPTION']['~VALUE'] ?>
+                            </p>
+                        </div>
+                    <?php else: ?>
+                        <div class="advantages-cards__item">
+                            <div class="advantages-cards__img">
+                                <img width="90" height="90" src="<?= CFile::GetPath($advantage['IMAGE']['VALUE']) ?>"
+                                     alt="<?= $advantage['DESCRIPTION']['~VALUE'] ?>">
+                            </div>
+                            <p class="advantages-cards__text">
+                                <?= $advantage['DESCRIPTION']['~VALUE'] ?>
+                            </p>
+                        </div>
+                    <?php endif ?>
+                <?php endforeach; ?>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
+
