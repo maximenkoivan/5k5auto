2c61ec0cab8e330ec081ec2e7528865688bd4ef8
maximenko <maximenkoivan83@gmail.com>
2024-07-24 19:23:27 +0300

    добавляет блок "обратная связь"

--- local/templates/5k5auto/components/bitrix/news.detail/main/_feedback.php
+++ local/templates/5k5auto/components/bitrix/news.detail/main/_feedback.php
@@ -1,40 +1,49 @@
 <?php
-
-use classes\Models\FiveKFiveAuto\Common\CommonData;
-use classes\Models\FiveKFiveAuto\Feedback\FormSettings;
-
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /**
  * @var $arResult
  * @global CMain $APPLICATION
  */
+$fileId = \classes\Models\FiveKFiveAuto\Common\CommonData::getInstance()->getPropertiesByCode('POLICY_FOOTER')->getResult()[0] ?: '';
+$formSettings = \classes\Models\FiveKFiveAuto\Feedback\FormSettings::getInstance()->getElementByCode('settings')->getResult();
 ?>
 <section class="application">
     <div class="container">
-        <h2 class="application__title">Оставьте заявку</h2>
+        <h2 class="application__title"><?= $arResult['PROPERTIES']['TITLE_FEEDBACK']['~VALUE'] ?></h2>
         <div class="application-order">
             <div class="application-order__form">
-                <h3 class="application-order__title">Сделай шаг на встречу автомобилю своей мечты!</h3>
-                <p class="application-order__text">Остальное мы возьмем на себя. Заполни данную форму, и мы перезвоним тебе
-                    в ближайшее время.</p>
-                <form class="application-order__select select-form" action="/callback.php" method="POST"
-                      data-success-header="Заявка отправлена"
-                      data-success-message="Спасибо за заполнение данной формы. Мы свяжется с вами в ближайшее время.">
+                <h3 class="application-order__title"><?= $arResult['PROPERTIES']['SUBTITLE_FEEDBACK']['~VALUE'] ?></h3>
+                <p class="application-order__text">
+                    <?= $arResult['PROPERTIES']['TEXT_FEEDBACK']['~VALUE'] ?>
+                </p>
+                <form class="application-order__select select-form" action="/request/feedback.php" method="POST"
+                      data-success-header="<?= $formSettings['TITLE_OK']['~VALUE'] ?>"
+                      data-success-message="<?= $formSettings['TEXT_OK']['~VALUE'] ?>">
                     <div id="orderForm" class="select-form__input select-form__input_name">
-                        <input type="text" id="name" name="name" placeholder="Имя">
+                        <input type="text" id="name" name="name"
+                               placeholder="<?= $formSettings['PLH_1_CALLBACK']['~VALUE'] ?>">
                     </div>
                     <div class="select-form__input select-form__input_phone">
-                        <input type="tel" id="phone" name="phone" placeholder="+7 (000) 000 00 00">
+                        <input type="tel" id="phone" name="phone"
+                               placeholder="<?= $formSettings['PLH_1_CALLBACK']['~VALUE'] ?>">
                     </div>
-                    <button class="select-form__btn" type="submit">Отправить заявку</button>
+                    <button class="select-form__btn"
+                            type="submit"><?= $formSettings['TEXT_BTN_CALLBACK']['~VALUE'] ?></button>
                 </form>
-                <p class="application-order__text application-order__text_policy">Нажимая на данную кнопку, вы даете
-                    согласие на обработку персональных
-                    данных и подтверждает факт ознакомления с действующей <a href="#" target="_blank"
-                                                                             rel="noopener noreferrer">Политикой конфиденциальности.</a></p>
+                <?php if (!empty($fileId) && !empty($formSettings['TEXT_CONSENT']['~VALUE']) && !empty($formSettings['TEXT_CONSENT']['~DESCRIPTION'])): ?>
+                    <p class="application-order__text application-order__text_policy">
+                        <?= $formSettings['TEXT_CONSENT']['~VALUE'] ?>
+                        <a href="<?= CFile::GetPath($fileId) ?>" target="_blank"
+                           rel="noopener noreferrer"><?= $formSettings['TEXT_CONSENT']['~DESCRIPTION'] ?></a>
+                    </p>
+                <?php endif; ?>
             </div>
             <div class="application-order__img">
-                <img width="480" height="740" src="images/application_img.jpg" alt="Изображение">
+                <?php if (!empty($arResult['PROPERTIES']['IMAGE_FEEDBACK']['VALUE'])): ?>
+                    <img width="480" height="740"
+                         src="<?= CFile::GetPath($arResult['PROPERTIES']['IMAGE_FEEDBACK']['VALUE']) ?>"
+                         alt="<?= $arResult['PROPERTIES']['IMAGE_FEEDBACK']['DESCRIPTION'] ?>">
+                <?php endif; ?>
             </div>
         </div>
     </div>
