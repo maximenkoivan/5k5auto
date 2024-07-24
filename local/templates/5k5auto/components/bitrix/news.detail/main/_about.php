4f5ce8af4b096f5d6bf2df6bf5d60a7c354af379
maximenko <maximenkoivan83@gmail.com>
2024-07-24 14:52:42 +0300

    добавляет блок "о компании"

--- local/templates/5k5auto/components/bitrix/news.detail/main/_about.php
+++ local/templates/5k5auto/components/bitrix/news.detail/main/_about.php
@@ -4,47 +4,66 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
  * @var $arResult
  * @global CMain $APPLICATION
  */
+$videoId = \classes\Helpers\Generic::getYoutubeData($arResult['PROPERTIES']['YOUTUBE_LINK_ABOUT']['~VALUE'])['VIDEO'] ?: '';
 ?>
 <section class="information next-section">
     <div class="container">
-        <h2 class="information__title">О компании</h2>
+        <h2 class="information__title"><?= $arResult['PROPERTIES']['TITLE_ABOUT']['~VALUE'] ?></h2>
         <div class="information__cards information-cards">
-            <div class="information-cards__item">
-                <div class="information-cards__content">
-                    <div class="information-cards__number" id="card-years">10+</div>
-                    <p class="information-cards__text">лет на рынке</p>
+            <?php if (!empty($arResult['PROPERTIES']['NUMBER_1_ABOUT']['~VALUE']) && !empty($arResult['PROPERTIES']['DESC_1_ABOUT']['~VALUE'])): ?>
+                <div class="information-cards__item">
+                    <div class="information-cards__content">
+                        <div class="information-cards__number"
+                             id="card-years"><?= $arResult['PROPERTIES']['NUMBER_1_ABOUT']['~VALUE'] ?></div>
+                        <p class="information-cards__text"><?= $arResult['PROPERTIES']['DESC_1_ABOUT']['~VALUE'] ?></p>
+                    </div>
+                    <?php if (!empty($arResult['PROPERTIES']['IMAGE_1_ABOUT']['~VALUE'])): ?>
+                        <img class="information-cards__img" width="338" height="425"
+                             src="<?= CFile::GetPath($arResult['PROPERTIES']['IMAGE_1_ABOUT']['~VALUE']) ?>"
+                             alt="<?= $arResult['PROPERTIES']['IMAGE_1_ABOUT']['DESCRIPTION'] ?>">
+                    <?php endif; ?>
                 </div>
-                <img class="information-cards__img" width="338" height="425" src="images/information-cards_1.jpg"
-                     alt="Изображение">
-            </div>
-            <div class="information-cards__item">
-                <div class="information-cards__content">
-                    <div class="information-cards__number" id="card-projects">200+</div>
-                    <p class="information-cards__text">Выполненных проектов разной сложности</p>
+            <?php endif; ?>
+            <?php if (!empty($arResult['PROPERTIES']['NUMBER_2_ABOUT']['~VALUE']) && !empty($arResult['PROPERTIES']['DESC_2_ABOUT']['~VALUE'])): ?>
+                <div class="information-cards__item">
+                    <div class="information-cards__content">
+                        <div class="information-cards__number"
+                             id="card-projects"><?= $arResult['PROPERTIES']['NUMBER_2_ABOUT']['~VALUE'] ?></div>
+                        <p class="information-cards__text"><?= $arResult['PROPERTIES']['DESC_2_ABOUT']['~VALUE'] ?></p>
+                    </div>
+                    <?php if (!empty($arResult['PROPERTIES']['IMAGE_2_ABOUT']['~VALUE'])): ?>
+                        <img class="information-cards__img" width="338" height="425"
+                             src="<?= CFile::GetPath($arResult['PROPERTIES']['IMAGE_2_ABOUT']['~VALUE']) ?>"
+                             alt="<?= $arResult['PROPERTIES']['IMAGE_2_ABOUT']['DESCRIPTION'] ?>">
+                    <?php endif; ?>
                 </div>
-                <img class="information-cards__img" width="338" height="425" src="images/information-cards_2.jpg"
-                     alt="Изображение">
-            </div>
-            <div class="information-cards__item">
-                <div class="information-cards__content">
-                    <div class="information-cards__number" id="card-specialists">14</div>
-                    <p class="information-cards__text">лет на рынке</p>
+            <?php endif; ?>
+            <?php if (!empty($arResult['PROPERTIES']['NUMBER_3_ABOUT']['~VALUE']) && !empty($arResult['PROPERTIES']['DESC_3_ABOUT']['~VALUE'])): ?>
+                <div class="information-cards__item">
+                    <div class="information-cards__content">
+                        <div class="information-cards__number"
+                             id="card-specialists"><?= $arResult['PROPERTIES']['NUMBER_3_ABOUT']['~VALUE'] ?></div>
+                        <p class="information-cards__text"><?= $arResult['PROPERTIES']['DESC_3_ABOUT']['~VALUE'] ?></p>
+                    </div>
+                    <?php if (!empty($arResult['PROPERTIES']['IMAGE_3_ABOUT']['~VALUE'])): ?>
+                        <img class="information-cards__img" width="338" height="425"
+                             src="<?= CFile::GetPath($arResult['PROPERTIES']['IMAGE_3_ABOUT']['~VALUE']) ?>"
+                             alt="<?= $arResult['PROPERTIES']['IMAGE_3_ABOUT']['DESCRIPTION'] ?>">
+                    <?php endif; ?>
                 </div>
-                <img class="information-cards__img" width="338" height="425" src="images/information-cards_3.jpg"
-                     alt="Изображение">
-            </div>
+            <?php endif; ?>
         </div>
-        <div class="information__info info">
-            <p class="info__text">Автоцентр «Auto 5K5» — это команда профессионалов с более чем 10 летним опытом. Мы
-                специализируемся на широком спектре услуг в области детейлинга автомобилей, гарантируя
-                высокое качество и результат. Разделяя вашу любовь к своему автомобилю, мы внимательно относимся к каждой
-                его детали. Используя в своей работе только самые лучшие и проверенные материалы, мы сохраняем блеск и
-                яркость вашего авто. С нами ваш автомобиль всегда будет как новый, а вы всегда будете
-                вспоминать момент его приобретения.
-            </p>
-            <div class="info__video" data-video="https://www.youtube.com/watch?v=1Rz3lHLOuYo">
-                <img src="//img.youtube.com/vi/1Rz3lHLOuYo/maxresdefault.jpg" alt="Обложка">
+        <?php if (!empty($arResult['PROPERTIES']['TEXT_ABOUT']['~VALUE']['TEXT'])): ?>
+            <div class="information__info info">
+                <p class="info__text">
+                    <?= $arResult['PROPERTIES']['TEXT_ABOUT']['~VALUE']['TEXT'] ?>
+                </p>
+                <?php if (!empty($videoId)): ?>
+                    <div class="info__video" data-video="https://www.youtube.com/watch?v=<?= $videoId ?>">
+                        <img src="//img.youtube.com/vi/<?= $videoId ?>/maxresdefault.jpg" alt="preview">
+                    </div>
+                <?php endif; ?>
             </div>
-        </div>
+        <?php endif; ?>
     </div>
 </section>
