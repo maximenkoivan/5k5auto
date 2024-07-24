7097b1a8cbf9fc9c8f2a56874c4a4dc2621f591b
maximenko <maximenkoivan83@gmail.com>
2024-07-24 15:58:25 +0300

    добавляет блок "получить консультацию"

--- local/templates/5k5auto/components/bitrix/news.detail/main/_consultation.php
+++ local/templates/5k5auto/components/bitrix/news.detail/main/_consultation.php
@@ -4,25 +4,33 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
  * @var $arResult
  * @global CMain $APPLICATION
  */
+$fileId = \classes\Models\FiveKFiveAuto\Common\CommonData::getInstance()->getPropertiesByCode('POLICY_FOOTER')->getResult()[0] ?: ''
 ?>
 <section class="consultation">
     <div class="container">
-        <h2 class="consultation__title">Получить консультацию</h2>
+        <h2 class="consultation__title"><?= $arResult['PROPERTIES']['TITLE_CONSULTATION']['~VALUE'] ?></h2>
         <div class="consultation__offer offer">
             <div class="offer__content">
-                <h3 class="offer__title">Узнай, как сохранить блеск и яркость своего автомобиля</h3>
-                <p class="offer__text">Оставь свой контактный телефон, и мы перезвоним тебе в ближайшее время.</p>
-                <button class="offer__btn" data-src="#modal-call">Оставить заявку</button>
-                <p class="offer__text offer__text_policy">Нажимая на данную кнопку, вы даете
-                    согласие на обработку персональных
-                    данных и подтверждает факт ознакомления с действующей <a href="#" target="_blank"
-                                                                             rel="noopener noreferrer">Политикой
-                        конфиденциальности.</a></p>
+                <h3 class="offer__title"><?= $arResult['PROPERTIES']['SUBTITLE_CONSULTATION']['~VALUE'] ?></h3>
+                <p class="offer__text"><?= $arResult['PROPERTIES']['TEXT_CONSULTATION']['~VALUE'] ?></p>
+                <?php if (!empty($arResult['PROPERTIES']['TEXT_BTN_CONSULTATION']['~VALUE'])): ?>
+                    <button class="offer__btn"
+                            data-src="#modal-call"><?= $arResult['PROPERTIES']['TEXT_BTN_CONSULTATION']['~VALUE'] ?></button>
+                <?php endif; ?>
+                <?php if (!empty($arResult['PROPERTIES']['TEXT_CONSENT_CONSULTATION']['~VALUE']) && !empty($arResult['PROPERTIES']['TEXT_CONSENT_CONSULTATION']['~DESCRIPTION']) && !empty($fileId)): ?>
+                    <p class="offer__text offer__text_policy">
+                        <?= $arResult['PROPERTIES']['TEXT_CONSENT_CONSULTATION']['~VALUE'] ?>
+                        <a href="<?= CFile::GetPath($fileId) ?>"
+                           target="_blank"><?= $arResult['PROPERTIES']['TEXT_CONSENT_CONSULTATION']['~DESCRIPTION'] ?></a>
+                    </p>
+                <?php endif; ?>
             </div>
-
-            <div class="offer__image">
-                <img src="images/offer_img.png" alt="Машина">
-            </div>
+            <?php if (!empty($arResult['PROPERTIES']['IMAGE_CONSULTATION']['VALUE'])): ?>
+                <div class="offer__image">
+                    <img src="<?= CFile::GetPath($arResult['PROPERTIES']['IMAGE_CONSULTATION']['VALUE']) ?>"
+                         alt="<?= $arResult['PROPERTIES']['IMAGE_CONSULTATION']['DESCRIPTION'] ?>">
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 </section>
