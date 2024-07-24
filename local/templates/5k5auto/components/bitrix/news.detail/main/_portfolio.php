d41d23f1c47c788080dccc4e38bc2ba6001cc6de
maximenko <maximenkoivan83@gmail.com>
2024-07-24 18:31:20 +0300

    добавляет блок "портфолио"

--- local/templates/5k5auto/components/bitrix/news.detail/main/_portfolio.php
+++ local/templates/5k5auto/components/bitrix/news.detail/main/_portfolio.php
@@ -1,137 +1,63 @@
 <?php
+
+use classes\Helpers\Generic;
+
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /**
  * @var $arResult
  * @global CMain $APPLICATION
  */
+$slides = \classes\Models\FiveKFiveAuto\Content\Portfolio::getInstance()->getElements()->getResult();
 ?>
-<section class="portfolio">
-    <div class="container">
-        <h2 class="portfolio__title">Портфолио</h2>
-        <div class="portfolio__slider">
-            <div class="swiper portfolio-slider">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <div class="portfolio-card">
-                            <div class="portfolio-card__content">
-                                <div class="portfolio-card__video" data-video="https://www.youtube.com/watch?v=1Rz3lHLOuYo">
-                                    <img src="//img.youtube.com/vi/1Rz3lHLOuYo/maxresdefault.jpg" alt="Обложка">
-                                </div>
-
-                                <!-- картинка -->
-
-                                <!-- <a class="portfolio-card__video" href="//img.youtube.com/vi/1Rz3lHLOuYo/maxresdefault.jpg"
-                                  data-fancybox="gallery">
-                                  <img src="//img.youtube.com/vi/1Rz3lHLOuYo/maxresdefault.jpg">
-                                </a> -->
-
-                                <div class="portfolio-card__images gallery">
-                                    <a class="gallery__item" href="//img.youtube.com/vi/LioNP7EQB0g/maxresdefault.jpg"
-                                       data-fancybox="gallery">
-                                        <img src="//img.youtube.com/vi/LioNP7EQB0g/maxresdefault.jpg">
-                                    </a>
-                                    <a class="gallery__item" href="//img.youtube.com/vi/kQ1ogy-SFEU/maxresdefault.jpg"
-                                       data-fancybox="gallery">
-                                        <img src="//img.youtube.com/vi/kQ1ogy-SFEU/maxresdefault.jpg">
-                                    </a>
-                                    <a class="gallery__item" href="//img.youtube.com/vi/71frqXB8yEA/maxresdefault.jpg"
-                                       data-fancybox="gallery">
-                                        <img src="//img.youtube.com/vi/71frqXB8yEA/maxresdefault.jpg">
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="portfolio-card__signature">Полная оклейка VOYAH FREE в матовую полиуретановую плёнку Satin
-                                matt</div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="portfolio-card">
-                            <div class="portfolio-card__content">
-                                <div class="portfolio-card__video" data-video="https://www.youtube.com/watch?v=1Rz3lHLOuYo">
-                                    <img src="//img.youtube.com/vi/1Rz3lHLOuYo/maxresdefault.jpg" alt="Обложка">
-                                </div>
-
-                                <!-- картинка -->
-
-                                <!-- <a class="portfolio-card__video" href="//img.youtube.com/vi/1Rz3lHLOuYo/maxresdefault.jpg" data-fancybox="gallery"> <img src="//img.youtube.com/vi/1Rz3lHLOuYo/maxresdefault.jpg"> </a> -->
-
-                                <div class="portfolio-card__images gallery">
-                                    <a class="gallery__item" href="//img.youtube.com/vi/LioNP7EQB0g/maxresdefault.jpg"
-                                       data-fancybox="gallery">
-                                        <img src="//img.youtube.com/vi/LioNP7EQB0g/maxresdefault.jpg">
-                                    </a>
-                                    <a class="gallery__item" href="//img.youtube.com/vi/kQ1ogy-SFEU/maxresdefault.jpg"
-                                       data-fancybox="gallery">
-                                        <img src="//img.youtube.com/vi/kQ1ogy-SFEU/maxresdefault.jpg">
-                                    </a>
-                                    <a class="gallery__item" href="//img.youtube.com/vi/71frqXB8yEA/maxresdefault.jpg"
-                                       data-fancybox="gallery">
-                                        <img src="//img.youtube.com/vi/71frqXB8yEA/maxresdefault.jpg">
-                                    </a>
+<?php if (!empty($slides)): ?>
+    <section class="portfolio">
+        <div class="container">
+            <h2 class="portfolio__title"><?= $arResult['PROPERTIES']['TITLE_PORTFOLIO']['~VALUE'] ?></h2>
+            <div class="portfolio__slider">
+                <div class="swiper portfolio-slider">
+                    <div class="swiper-wrapper">
+                        <?php foreach ($slides as $slide): ?>
+                            <?php if (empty($slide['LINK']['~VALUE']) && empty($slide['IMAGES']['VALUE'])) continue ?>
+                            <div class="swiper-slide">
+                                <div class="portfolio-card">
+                                    <div class="portfolio-card__content">
+                                        <?php if (!empty($slide['LINK']['~VALUE'])): ?>
+                                            <div class="portfolio-card__video"
+                                                 data-video="<?= $slide['LINK']['~VALUE'] ?>">
+                                                <img src="//img.youtube.com/vi/<?= Generic::getYoutubeData($slide['LINK']['~VALUE'])['VIDEO'] ?>/maxresdefault.jpg"
+                                                     alt="<?= $slide['DESCRIPTION']['~VALUE'] ?>">
+                                            </div>
+                                        <?php else: ?>
+                                            <a class="portfolio-card__img"
+                                               href="<?= CFile::GetPath($slide['IMAGES']['VALUE'][0]) ?>"
+                                               data-fancybox="gallery">
+                                                <img src="<?= CFile::GetPath($slide['IMAGES']['VALUE'][0]) ?>"
+                                                     alt="<?= $slide['IMAGES']['DESCRIPTION'][0] ?>">
+                                            </a>
+                                        <?php endif; ?>
+                                        <div class="portfolio-card__images gallery">
+                                            <?php foreach ($slide['IMAGES']['VALUE'] as $key => $imageId): ?>
+                                                <?php if ($key > 3 || (empty($slide['LINK']['~VALUE']) && $key === 0)) continue ?>
+                                                <a class="gallery__item"
+                                                   href="<?= CFile::GetPath($imageId) ?>"
+                                                   data-fancybox="gallery">
+                                                    <img src="<?= CFile::GetPath($imageId) ?>"
+                                                         alt="<?= $slide['IMAGES']['DESCRIPTION'][$key] ?>">
+                                                </a>
+                                            <?php endforeach; ?>
+                                        </div>
+                                    </div>
+                                    <div class="portfolio-card__signature">
+                                        <?= $slide['DESCRIPTION']['~VALUE'] ?>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="portfolio-card__signature">Оклейка глянцевых элементов в полиуритановую пленку QUANTUM PRO
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="portfolio-card">
-                            <div class="portfolio-card__content">
-                                <div class="portfolio-card__video" data-video="https://www.youtube.com/watch?v=1Rz3lHLOuYo">
-                                    <img src="//img.youtube.com/vi/1Rz3lHLOuYo/maxresdefault.jpg" alt="Обложка">
-                                </div>
-
-                                <!-- картинка -->
-
-                                <!-- <a class="portfolio-card__video" href="//img.youtube.com/vi/1Rz3lHLOuYo/maxresdefault.jpg"
-                                                                                        data-fancybox="gallery">
-                                                                                        <img src="//img.youtube.com/vi/1Rz3lHLOuYo/maxresdefault.jpg">
-                                                                                      </a> -->
-
-                                <div class="portfolio-card__images gallery">
-                                    <a class="gallery__item" href="//img.youtube.com/vi/LioNP7EQB0g/maxresdefault.jpg"
-                                       data-fancybox="gallery">
-                                        <img src="//img.youtube.com/vi/LioNP7EQB0g/maxresdefault.jpg">
-                                    </a>
-                                    <a class="gallery__item" href="//img.youtube.com/vi/kQ1ogy-SFEU/maxresdefault.jpg"
-                                       data-fancybox="gallery">
-                                        <img src="//img.youtube.com/vi/kQ1ogy-SFEU/maxresdefault.jpg">
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="portfolio-card__signature">Оклейка глянцевых элементов в полиуритановую пленку QUANTUM PRO
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="portfolio-card">
-                            <div class="portfolio-card__content">
-                                <div class="portfolio-card__video" data-video="https://www.youtube.com/watch?v=1Rz3lHLOuYo">
-                                    <img src="//img.youtube.com/vi/1Rz3lHLOuYo/maxresdefault.jpg" alt="Обложка">
-                                </div>
-
-                                <!-- картинка -->
-
-                                <!-- <a class="portfolio-card__video" href="//img.youtube.com/vi/1Rz3lHLOuYo/maxresdefault.jpg"
-                                                                                        data-fancybox="gallery">
-                                                                                        <img src="//img.youtube.com/vi/1Rz3lHLOuYo/maxresdefault.jpg">
-                                                                                      </a> -->
-
-                                <div class="portfolio-card__images gallery">
-                                    <a class="gallery__item" href="//img.youtube.com/vi/LioNP7EQB0g/maxresdefault.jpg"
-                                       data-fancybox="gallery">
-                                        <img src="//img.youtube.com/vi/LioNP7EQB0g/maxresdefault.jpg">
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="portfolio-card__signature">Оклейка глянцевых элементов в полиуритановую пленку QUANTUM PRO
-                            </div>
-                        </div>
+                        <?php endforeach; ?>
                     </div>
                 </div>
+                <button class="swiper-button-prev" tabindex="0"></button>
+                <button class="swiper-button-next" tabindex="0"></button>
             </div>
-            <button class="swiper-button-prev" tabindex="0"></button>
-            <button class="swiper-button-next" tabindex="0"></button>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
