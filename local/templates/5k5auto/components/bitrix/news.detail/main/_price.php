e7058eb05289e9e09d5657a340f8330cb6e3a2d8
maximenko <maximenkoivan83@gmail.com>
2024-07-24 17:29:57 +0300

    добавляет блок "прайс"

--- local/templates/5k5auto/components/bitrix/news.detail/main/_price.php
+++ local/templates/5k5auto/components/bitrix/news.detail/main/_price.php
@@ -1,76 +1,45 @@
 <?php
+
+use classes\Helpers\FilesHelper;
+
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /**
  * @var $arResult
  * @global CMain $APPLICATION
  */
+$services = \classes\Models\FiveKFiveAuto\Content\Price::getInstance()->getElements()->getResult();
 ?>
-<section class="price-list">
-    <div class="container">
-        <h2 class="price-list__title">Прайс-лист</h2>
-        <div class="price-list__items price-cards">
-            <div class="price-cards__item">
-                <div class="price-cards__img">
-                    <img width="380" height="170" src="images/service_1.jpg" alt="Картинка">
-                </div>
-                <h3 class="price-cards__cost">от 300 000 руб.</h3>
-                <p class="price-cards__name">Оклейка полная LLUMAR/Santec</p>
+<?php if (!empty($services)): ?>
+    <section class="price-list">
+        <div class="container">
+            <h2 class="price-list__title"><?= $arResult['PROPERTIES']['TITLE_PRICE']['~VALUE'] ?></h2>
+            <div class="price-list__items price-cards">
+                <?php foreach ($services as $service): ?>
+                    <div class="price-cards__item">
+                        <div class="price-cards__img">
+                            <img width="380" height="170"
+                                 src="<?= FilesHelper::getImageById($service['IMAGE']['VALUE']) ?>"
+                                 alt="<?= $service['NAME'] ?>">
+                        </div>
+                        <h3 class="price-cards__cost"><?= $service['PRICE']['~VALUE'] ?></h3>
+                        <p class="price-cards__name"><?= $service['NAME'] ?></p>
+                    </div>
+                <?php endforeach; ?>
             </div>
-            <div class="price-cards__item">
-                <div class="price-cards__img">
-                    <img width="380" height="170" src="images/service_2.jpg" alt="Картинка">
-                </div>
-                <h3 class="price-cards__cost">от 250 000 руб.</h3>
-                <p class="price-cards__name">Оклейка полная Spectroll</p>
-            </div>
-            <div class="price-cards__item">
-                <div class="price-cards__img">
-                    <img width="380" height="170" src="images/service_3.jpg" alt="Картинка">
-                </div>
-                <h3 class="price-cards__cost">от 90 000 руб.</h3>
-                <p class="price-cards__name">Оклейка зон риска LLUMAR/Santec</p>
-            </div>
-            <div class="price-cards__item">
-                <div class="price-cards__img">
-                    <img width="380" height="170" src="images/service_4.jpg" alt="Картинка">
-                </div>
-                <h3 class="price-cards__cost">от 65 000 руб.</h3>
-                <p class="price-cards__name">Оклейка зон риска Spectroll</p>
-            </div>
-            <div class="price-cards__item">
-                <div class="price-cards__img">
-                    <img width="380" height="170" src="images/service_5.jpg" alt="Картинка">
-                </div>
-                <h3 class="price-cards__cost">от 3 000 руб.</h3>
-                <p class="price-cards__name">Оклейка 1 элемента</p>
-            </div>
-            <div class="price-cards__item">
-                <div class="price-cards__img">
-                    <img width="380" height="170" src="images/service_6.jpg" alt="Картинка">
-                </div>
-                <h3 class="price-cards__cost">от 500 руб.</h3>
-                <p class="price-cards__name">Снятие пленки</p>
-            </div>
-            <div class="price-cards__item">
-                <div class="price-cards__img">
-                    <img width="380" height="170" src="images/service_7.jpg" alt="Картинка">
-                </div>
-                <h3 class="price-cards__cost">от 65 000 руб.</h3>
-                <p class="price-cards__name">Оклейка зон риска Spectroll</p>
-            </div>
-            <div class="price-cards__item">
-                <div class="price-cards__img">
-                    <img width="380" height="170" src="images/service_8.jpg" alt="Картинка">
-                </div>
-                <h3 class="price-cards__cost">от 3 000 руб.</h3>
-                <p class="price-cards__name">Оклейка 1 элемента</p>
+            <div class="price-list__selects selects-buttons">
+                <?php if (!empty($arResult['PROPERTIES']['TEXT_BTN_PRICE']['VALUE'])): ?>
+                    <button class="selects-buttons__btn"
+                            data-src="#modal-call"><?= $arResult['PROPERTIES']['TEXT_BTN_PRICE']['VALUE'] ?>
+                    </button>
+                <?php endif; ?>
+                <?php if (!empty($arResult['PROPERTIES']['FILE_PRICE']['VALUE']) && !empty($arResult['PROPERTIES']['FILE_PRICE']['DESCRIPTION'])): ?>
+                    <a class="selects-buttons__link"
+                       href="<?= CFile::GetPath($arResult['PROPERTIES']['FILE_PRICE']['VALUE']) ?>" target="_blank"
+                       rel="noopener noreferrer"><?= $arResult['PROPERTIES']['FILE_PRICE']['DESCRIPTION'] ?>
+                    </a>
+                <?php endif; ?>
             </div>
         </div>
+    </section>
+<?php endif; ?>
 
-        <div class="price-list__selects selects-buttons">
-            <button class="selects-buttons__btn" data-src="#modal-call">Получить консультацию</button>
-            <a class="selects-buttons__link" href="#" target="_blank" rel="noopener noreferrer">Скачать прайс-лист</a>
-        </div>
-
-    </div>
-</section>
