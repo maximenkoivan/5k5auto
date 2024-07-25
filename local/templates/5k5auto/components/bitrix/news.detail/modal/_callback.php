c9f84b93e3accd8288fe21424909742f01cf2e33
maximenko <maximenkoivan83@gmail.com>
2024-07-25 13:50:58 +0300

    добавляет модалку callback

--- local/templates/5k5auto/components/bitrix/news.detail/modal/_callback.php
+++ local/templates/5k5auto/components/bitrix/news.detail/modal/_callback.php
@@ -2,26 +2,30 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
+/**
+ * @var $arResult
+ * @global CMain $APPLICATION
+ */
+$fileId = \classes\Models\FiveKFiveAuto\Common\CommonData::getInstance()->getPropertiesByCode('POLICY_FOOTER')->getResult()[0] ?: '';
 ?>
 <div id="modal-call" class="modal-call">
     <div class="container">
         <div class="modal-call__form">
-            <h2 class="modal-call__title">Заполните форму и<br> мы свяжемся с вами<br> в ближайшее время</h2>
-            <form class="modal-call__select select-form" action="/request/feedback.php" method="POST"
-                  data-success-header="Заявка отправлена"
-                  data-success-message="Спасибо за заполнение данной формы. Мы свяжется с вами в ближайшее время.">
+            <h2 class="modal-call__title"><?= $arResult['PROPERTIES']['TITLE_CALLBACK']['~VALUE'] ?></h2>
+            <form class="modal-call__select select-form" action="/request/feedback.php" method="POST">
                 <div id="orderForm" class="select-form__input select-form__input_name">
-                    <input type="text" id="name" name="name" placeholder="Имя">
+                    <input type="text" id="name" name="name" placeholder="<?= $arResult['PROPERTIES']['PLH_1_CALLBACK']['~VALUE'] ?>">
                 </div>
                 <div class="select-form__input select-form__input_phone">
-                    <input type="tel" id="phone" name="phone" placeholder="+7 (000) 000 00 00">
+                    <input type="tel" id="phone" name="phone" placeholder="<?= $arResult['PROPERTIES']['PLH_2_CALLBACK']['~VALUE'] ?>">
                 </div>
-                <button class="select-form__btn" type="submit">Отправить заявку</button>
+                <button class="select-form__btn" type="submit"><?= $arResult['PROPERTIES']['TEXT_BTN_CALLBACK']['~VALUE'] ?></button>
             </form>
-            <p class="modal-call__text modal-call__text_policy">Нажимая на данную кнопку, вы даете
-                согласие на обработку персональных
-                данных и подтверждает факт ознакомления с действующей <a href="#" target="_blank"
-                                                                         rel="noopener noreferrer">Политикой конфиденциальности.</a></p>
+            <?php if (!empty($fileId) && !empty($arResult['PROPERTIES']['TEXT_CONSENT']['~VALUE']) && !empty($arResult['PROPERTIES']['TEXT_CONSENT']['~DESCRIPTION'])): ?>
+            <p class="modal-call__text modal-call__text_policy">
+                <?= $arResult['PROPERTIES']['TEXT_CONSENT']['~VALUE'] ?>
+                <a href="<?= CFile::GetPath($fileId) ?>" target="_blank" rel="noopener noreferrer"><?= $arResult['PROPERTIES']['TEXT_CONSENT']['~DESCRIPTION'] ?></a></p>
+            <?php endif; ?>
         </div>
     </div>
 </div>
--- local/templates/5k5auto/components/bitrix/news.detail/modal/_ok.php
+++ local/templates/5k5auto/components/bitrix/news.detail/modal/_ok.php
@@ -2,12 +2,19 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
+/**
+ * @var $arResult
+ * @global CMain $APPLICATION
+ */
 ?>
 <div id="modal-thnx" class="modal-thnx">
     <div class="container">
-        <h2 class="modal-thnx__title">Спасибо за<br> заявку. Мы скоро<br> свяжемся с вами!</h2>
-        <div class="modal-thnx__img">
-            <img width="701" height="396" src="images/modal-car-img.png" alt="Машина">
-        </div>
+        <h2 class="modal-thnx__title"><?= $arResult['PROPERTIES']['TITLE_OK']['~VALUE'] ?></h2>
+        <?php if (!empty($arResult['PROPERTIES']['IMAGE_OK']['~VALUE'])): ?>
+            <div class="modal-thnx__img">
+                <img width="701" height="396" src="<?= CFile::GetPath($arResult['PROPERTIES']['IMAGE_OK']['~VALUE']) ?>"
+                     alt="<?= $arResult['PROPERTIES']['IMAGE_OK']['DESCRIPTION'] ?>">
+            </div>
+        <?php endif; ?>
     </div>
 </div>
