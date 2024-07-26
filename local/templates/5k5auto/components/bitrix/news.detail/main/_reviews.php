9491806bfc0326588d382b9b68ea8fb5a3a1f495
maximenko <maximenkoivan83@gmail.com>
2024-07-26 13:55:17 +0300

    добавляет блок с отзывами

--- local/templates/5k5auto/components/bitrix/news.detail/main/_reviews.php
+++ local/templates/5k5auto/components/bitrix/news.detail/main/_reviews.php
@@ -4,72 +4,48 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
  * @var $arResult
  * @global CMain $APPLICATION
  */
+$reviews = \classes\Models\FiveKFiveAuto\Content\Reviews::getInstance()->getElements()->getResult();
 ?>
-<section class="reviews">
-    <div class="container">
-        <h2 class="reviews__title">Отзывы</h2>
-        <div class="reviews__slider">
-            <div class="swiper reviews-slider">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <div class="reviews-card">
-                            <div class="reviews-card__content">
-                                <div class="reviews-card__img">
-                                    <img width="550" height="356" src="images/slide_img_1.jpg" alt="Отзыв 1">
+<?php if (!empty($reviews)): ?>
+    <section class="reviews">
+        <div class="container">
+            <h2 class="reviews__title">Отзывы</h2>
+            <div class="reviews__slider">
+                <div class="swiper reviews-slider">
+                    <div class="swiper-wrapper">
+                        <?php foreach ($reviews as $review): ?>
+                            <?php if (empty($review['PHOTO_AUTO']['VALUE'])) continue ?>
+                            <div class="swiper-slide">
+                                <div class="reviews-card">
+                                    <div class="reviews-card__content">
+                                        <div class="reviews-card__img card-img">
+                                            <?php if (!empty($review['PHOTO_AUTO']['VALUE'])): ?>
+                                                <img class="card-img__car" width="550" height="356"
+                                                     src="<?= CFile::GetPath($review['PHOTO_AUTO']['VALUE']) ?>"
+                                                     alt="<?= $review['PHOTO_AUTO']['DESCRIPTION'] ?>">
+                                            <?php endif; ?>
+                                            <?php if (!empty($review['PHOTO_AUTHOR']['VALUE'])): ?>
+                                                <div class="card-img__avatar">
+                                                    <img class="card-img__user" width="130" height="130"
+                                                         src="<?= CFile::GetPath($review['PHOTO_AUTHOR']['VALUE']) ?>"
+                                                         alt="<?= $review['PHOTO_AUTHOR']['DESCRIPTION'] ?>">
+                                                </div>
+                                            <?php endif; ?>
+                                        </div>
+                                        <h3 class="reviews-card__title"><?= $review['PHOTO_AUTHOR']['DESCRIPTION'] ?></h3>
+                                        <p class="reviews-card__text">
+                                            <?= $review['~PREVIEW_TEXT'] ?>
+                                        </p>
+                                    </div>
+                                    <div class="reviews-card__signature"><?= $review['PHOTO_AUTO']['DESCRIPTION'] ?></div>
                                 </div>
-                                <h3 class="reviews-card__title">Андрей</h3>
-                                <p class="reviews-card__text">«Спасибо, Ребята! Вы настоящие профессионалы. Выполнили полную оклейку
-                                    моего автомобиля в
-                                    самые сжатые сроки. Результатом доволен»</p>
                             </div>
-                            <div class="reviews-card__signature">VOYAH FREE</div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="reviews-card">
-                            <div class="reviews-card__content">
-                                <div class="reviews-card__img">
-                                    <img width="550" height="356" src="images/slide_img_2.jpg" alt="Отзыв 2">
-                                </div>
-                                <h3 class="reviews-card__title">Сергей</h3>
-                                <p class="reviews-card__text">«Сняли пленку и избавили от небольших вмятен. Авто снова как новое.
-                                    Отлично, спасибо!»</p>
-                            </div>
-                            <div class="reviews-card__signature">BMW X5</div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="reviews-card">
-                            <div class="reviews-card__content">
-                                <div class="reviews-card__img">
-                                    <img width="550" height="356" src="images/slide_img_1.jpg" alt="Отзыв 1">
-                                </div>
-                                <h3 class="reviews-card__title">Андрей</h3>
-                                <p class="reviews-card__text">«Спасибо, Ребята! Вы настоящие профессионалы. Выполнили полную оклейку
-                                    моего автомобиля в
-                                    самые сжатые сроки. Результатом доволен»</p>
-                            </div>
-                            <div class="reviews-card__signature">VOYAH FREE</div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="reviews-card">
-                            <div class="reviews-card__content">
-                                <div class="reviews-card__img">
-                                    <img width="550" height="356" src="images/slide_img_2.jpg" alt="Отзыв 2">
-                                </div>
-                                <h3 class="reviews-card__title">Сергей</h3>
-                                <p class="reviews-card__text">«Сняли пленку и избавили от небольших вмятен. Авто снова как новое.
-                                    Отлично,
-                                    спасибо!»</p>
-                            </div>
-                            <div class="reviews-card__signature">BMW X5</div>
-                        </div>
+                        <?php endforeach; ?>
                     </div>
                 </div>
+                <button class="swiper-button-prev" tabindex="0"></button>
+                <button class="swiper-button-next" tabindex="0"></button>
             </div>
-            <button class="swiper-button-prev" tabindex="0"></button>
-            <button class="swiper-button-next" tabindex="0"></button>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
