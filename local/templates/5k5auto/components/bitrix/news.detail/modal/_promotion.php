7c3356fab1339a2c21ec723924df7a8acb4bb24a
maximenko <maximenkoivan83@gmail.com>
2024-07-26 13:59:06 +0300

    добавляет модалку с акцией

--- local/templates/5k5auto/components/bitrix/news.detail/modal/_promotion.php
+++ local/templates/5k5auto/components/bitrix/news.detail/modal/_promotion.php
@@ -11,16 +11,21 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
 <div id="modal-promotion" class="modal-promotion">
     <div class="container">
         <div class="modal-promotion__img">
-            <img width="496" height="509"
-                 src="<?= CFile::GetPath($arResult['PROPERTIES']['IMAGE_PROMOTION']['VALUE']) ?>"
-                 alt="<?= $arResult['PROPERTIES']['IMAGE_PROMOTION']['DESCRIPTION'] ?>">
+            <?php if (!empty($arResult['PROPERTIES']['IMAGE_PROMOTION']['VALUE'])): ?>
+                <img width="496" height="509"
+                     src="<?= CFile::GetPath($arResult['PROPERTIES']['IMAGE_PROMOTION']['VALUE']) ?>"
+                     alt="<?= $arResult['PROPERTIES']['IMAGE_PROMOTION']['DESCRIPTION'] ?>">
+            <?php endif; ?>
         </div>
         <div class="modal-promotion__content">
             <h2 class="modal-promotion__title"><?= $arResult['PROPERTIES']['TITLE_PROMOTION']['~VALUE'] ?></h2>
             <p class="modal-promotion__text">
                 <?= $arResult['PROPERTIES']['TEXT_PROMOTION']['~VALUE']['TEXT'] ?>
             </p>
-            <button class="modal-promotion__btn" data-src="#modal-call"><?= $arResult['PROPERTIES']['TEXT_BTN_PROMOTION']['VALUE'] ?></button>
+            <?php if (!empty($arResult['PROPERTIES']['TEXT_BTN_PROMOTION']['VALUE'])): ?>
+                <button class="modal-promotion__btn"
+                        data-src="#modal-call"><?= $arResult['PROPERTIES']['TEXT_BTN_PROMOTION']['VALUE'] ?></button>
+            <?php endif; ?>
         </div>
     </div>
 </div>
